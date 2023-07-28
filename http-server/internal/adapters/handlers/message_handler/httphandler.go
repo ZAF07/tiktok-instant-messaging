35a6c5e37964e6fec6185a0ba6edfea28f6a76c9
ZAF07 <zaffere07@gmail.com>
2023-07-28 10:18:37 +0800

    moved message_router modules to a single file

--- http-server/internal/adapters/handlers/message_handler/httphandler.go
+++ http-server/internal/adapters/handlers/message_handler/httphandler.go
@@ -1,13 +1,10 @@
-package httphandler
+package messagehandler
 
 import (
-	"log"
 	"net/http"
 
-	"github.com/ZAF07/tiktok-instant-messaging/http-server/config"
 	httpdomain "github.com/ZAF07/tiktok-instant-messaging/http-server/internal/core/domain/http_domain"
 	"github.com/ZAF07/tiktok-instant-messaging/http-server/internal/core/ports"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -46,25 +43,4 @@ func (h *HTTPHandler) Pull(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"msg": res,
 	})
-}
-
-func (h *HTTPHandler) InitRoutes(r *gin.Engine) {
-	config, err := config.GetConfig()
-	if err != nil {
-		log.Fatalf("error loading application config file: %v", err)
-	}
-	r.Use(cors.New(cors.Config{
-		AllowCredentials: false,
-		AllowOrigins:     config.GetCorsAllowOrigins(),
-		AllowHeaders:     config.GetHTTPAllowMethods(),
-		// AllowAllOrigins:  true,
-		// AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTION", "HEAD", "PATCH", "COMMON"},
-		// AllowHeaders: []string{"Content-Type", "Content-Length", "Authorization", "accept", "origin", "Referer", "User-Agent"},
-	}))
-
-	messages := r.Group("messages")
-	{
-		messages.GET("/push", h.Push)
-		messages.GET("/pull", h.Pull)
-	}
 }
---
+++ http-server/internal/adapters/handlers/message_handler/message_router.go
@@ -0,0 +1,30 @@
+package messagehandler
+
+import (
+	"log"
+
+	"github.com/ZAF07/tiktok-instant-messaging/http-server/config"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *HTTPHandler) InitRoutes(r *gin.Engine) {
+	config, err := config.GetConfig()
+	if err != nil {
+		log.Fatalf("error loading application config file: %v", err)
+	}
+	r.Use(cors.New(cors.Config{
+		AllowCredentials: false,
+		AllowOrigins:     config.GetCorsAllowOrigins(),
+		AllowHeaders:     config.GetHTTPAllowMethods(),
+		// AllowAllOrigins:  true,
+		// AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTION", "HEAD", "PATCH", "COMMON"},
+		// AllowHeaders: []string{"Content-Type", "Content-Length", "Authorization", "accept", "origin", "Referer", "User-Agent"},
+	}))
+
+	messages := r.Group("messages")
+	{
+		messages.GET("/push", h.Push)
+		messages.GET("/pull", h.Pull)
+	}
+}
